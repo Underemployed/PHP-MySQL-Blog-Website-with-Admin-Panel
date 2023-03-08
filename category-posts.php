de8847dfe40be57ed2e3b47485e3cb60f930d22a
Underemployed <underemployed871@gmail.com>
2023-03-08 10:38:08 +0630

    final commit

--- category-posts.php
+++ category-posts.php
@@ -27,44 +27,50 @@ if(isset($_GET['id'])){
 
 
 <?php if ((mysqli_num_rows($posts)) > 0) : ?>
-<section class="posts">
+<section class="posts <?= $featured ? '' : 'section__extra-margin' ?>">
+  <div class="container posts__container">
+    <?php while ($post = mysqli_fetch_assoc($posts)) : ?>
+      <article class="post">
+        <div class="post__thumbnail" style="width: 300px; height: 200px;">
+          <img src="./images/<?= $post['thumbnail'] ?>" >
+        </div>
+        <div class="post__info">
+          <?php // fetch category from categories using category_id
+          $category_id = $post['category_id'];
+          $category_query = "SELECT * FROM categories WHERE id=$category_id";
+          $category_result = mysqli_query($connection, $category_query);
+          $category = mysqli_fetch_assoc($category_result);
+          ?>
+          <a href="<?= ROOT_URL ?>category-posts.php?id=<?= $post['category_id'] ?>" class="category__button"><?= $category['title'] ?></a>
+          <h2 class="post__title"><a href="<?= ROOT_URL ?>post.php?id=<?= $post['id'] ?>"><?= $post['title'] ?></a></h2>
+          <a href="<?= ROOT_URL ?>post.php?id=<?= $post['id'] ?>">
 
-    <div class="container posts__container">
-    <?php while ($post=mysqli_fetch_assoc($posts)) :?>
-            <article class="post">
-                <div class="post__thumbnail">
-                    <img src="./images/<?=$post['thumbnail']?>" >
-                </div>
-                <div class="post__info">
-                <?php
+            <p class="post__body" style="min-height: 100px;">
+              <?= substr($post['body'], 0, 120) ?>...
+            </p>
+          </a>
 
-
-                $author_id=$post['author_id'];
-                $author_query="SELECT * FROM users WHERE id=$author_id";
-                $author_result=mysqli_query($connection,$author_query);
-                $author=mysqli_fetch_assoc($author_result);
-                            
-                ?>
-                    <h3 class="post__title"><a href="post.php?id=<?=$post["id"]?>" >
-                        <?=$post['title']?>
-                    </a></h3>
-                    <p class="post__body">
-                    <?= substr($post['body'],0,150) ?>...                    
-                    </p>
-                    <div class="post__author">
-                        <div class="post__author-avatar">
-                        <img src="./images/<?= $author['avatar'] ?>">
-                        </div>
-                        <div class="post__author-info">
-                            <h5>By: <?= "{$author['firstname']} {$author['lastname']}" ?></h5>
-                            <small>
-                            <?=date("M d, Y -H:i" , strtotime($post['date_time']))?>
-                            </small>
-                        </div>
-                    </div>
-                </div>
-
-            </article>
+          <div class="post__author">
+            <?php
+            // Fetch author from users table using author id
+            $author_id = $post['author_id'];
+            $author_query = "SELECT * FROM users WHERE id=$author_id";
+            $author_result = mysqli_query($connection, $author_query);
+            $author = mysqli_fetch_assoc($author_result);
+            $author_firstname = $author['firstname'];
+            $author_lastname = $author['lastname'];
+            ?>
+            <div class="post__author-avatar">
+              <img src="./images/<?= $author['avatar'] ?>" alt="" />
+            </div>
+            <div class="post__author-info">
+              <h5>By: <?= "{$author_firstname} {$author_lastname}" ?></h5>
+              <small><?= date("M d, Y - H:i", strtotime($post['date_time'])) ?></small>
+            </div>
+          </div>
+          </h3>
+        </div>
+      </article>
             <?php endwhile ?>
 
        
